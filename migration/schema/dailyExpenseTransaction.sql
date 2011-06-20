073d358e4e0094f9c8062fd406ade262921e4eef
Mark <mvb_marksman@yahoo.com>
2011-06-20 08:03:38 +0800

    - added schema definition, merged them into a single file - added an image containing the ERD

--- migration/schema/dailyExpenseTransaction.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE IF EXISTS `DailyExpenseTransaction`;
-
-CREATE TABLE `DailyExpenseTransaction` (
-  `dailyExpenseTransacationId` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `date` DATETIME NOT NULL ,
-  PRIMARY KEY (`dailyExpenseTransacationId`)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
