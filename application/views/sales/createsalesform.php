f266252e3eb8e77cddfb9d050075279946e7a856
Mark <mvb_marksman@yahoo.com>
2011-06-26 16:49:53 +0800

    added js to comput total

--- application/views/sales/createsalesform.php
+++ application/views/sales/createsalesform.php
@@ -33,9 +33,9 @@
 
 <!-- used as template for dynamically adding rows -->
 <div id="tpl" style="display:none">
-	<li>
+	<li id="row_1">
 		<ul class="sales_form_row">
-			<li>
+			<li class="itemDetails">
 				<select name="item" onchange="updatePrice(this)">
 					<?php foreach ($itemDetails as $itemDetail): ?>
 					<option value="<?php echo $itemDetail['itemDetailId'] ?>">
@@ -44,23 +44,36 @@
 					<?php endforeach; ?>
 				</select>
 			</li>
-			<li>
+			<li class="sellingPrice">
 				<?php echo $itemDetails[0]['sellingPrice'];?>
 			</li>
-				<li><input type="text" name="qty[]" size ="3" value="0" onblur="updateSubTotal(this)"/></li>
-				<li><input type="text" size ="6" name="discount[]" value="0.00"/></li>
-				<li><input type="checkbox" name="vat[]"/></li>
-			<li></li>
-			<li><input type="button" value="add" onclick="addRow(this)"></li>
+			<li class="quantity">
+				<input type="text" name="qty[]" size ="3" value="0" onblur="updateSubTotal(this)"/>
+			</li>
+			<li class="discount">
+				<input type="text" size ="6" name="discount[]" value="0.00"/>
+			</li>
+			<li class="vat">
+				<input type="checkbox" name="vat[]"/>
+			</li>
+			<li class="subtotal">
+				5
+			</li>
+			<li class="controls">
+				<input type="button" value="add" onclick="addRow(this)">
+			</li>
 		</ul>
 	</li>
 </div>
+
+<input type="button" onclick="computeTotal()">
 
 <script type="text/javascript">
 var priceLookup = new Array();
 <?php foreach ($itemDetails as $itemDetail):?>
 	priceLookup["<?php echo $itemDetail['itemDetailId']; ?>"] = "<?php echo $itemDetail['sellingPrice'] ?>";
 <? endforeach; ?>
+var ctr = 1;
 
 $(document).ready(function(){
 	$(".sales_form_container").append($("#tpl").html());
@@ -69,6 +82,15 @@ $(document).ready(function(){
 function addRow(obj) {
 	$(obj).attr("value", "remove");
 	$(obj).attr("onclick", "removeRow(this)");
+
+	// get the first li in the template and set the ID
+	var row = $("#tpl").children()[0];
+	$(row).attr("id", "row_" + ++ctr);
+
+	var cells = $(row).children().first().children().each(function(i){
+		this.id = this.className + "_" + ctr;
+	});
+
 	$(".sales_form_container").append($("#tpl").html());
 }
 
@@ -85,9 +107,14 @@ function updateSubTotal(obj){
 	alert("test");
 }
 
-
-
-
+function computeTotal() {
+	var total = 0;
+	$(".subtotal:visible").each(function(){
+		total += parseInt($(this).html());
+		return total;
+	});
+	console.log(total);
+}
 
 
 </script>
