b70ee8c84873bf56a3c68c8bf375172702de3c54
Erick Masanque <erickmasanque@gmail.com>
2011-07-03 12:44:37 +0800

    Completed all the functions needed to put the sales form values to the database using a fullpayment mode

--- application/controllers/sales.php
+++ application/controllers/sales.php
@@ -24,54 +24,44 @@ class Sales extends MY_Controller {
 		$sales_transaction->isFullyPaid = 1;//TODO CHANGED
 		$this->sales_transaction_model->save($sales_transaction);
 
-		debug :: dump($sales_transaction);
-
-/*		$sales_transaction= new Sales_transaction_model();
-		$sales_transaction->date = date("Y-m-d H:i:s");
-		$sales_transaction->userId = 1; //TODO CHANGED
-		$sales_transaction->isFullyPaid = 1;//TODO CHANGED
-		$this->sales_transaction_model->save($sales_transaction);
-
-
-		$qty = $this->input->post('qty');
 		$itemDetailId = $this->input->post('item');
+		$qty = $this->input->post('qty');
 		$salesTransactionId = $this->db->insert_id();
 		$discount = $this->input->post('discount');
 		$storeId = 1;  //TODO change this pls
-		$vat = $this->input->post('vat');
+		$vatquery = $this->input->post('vat');
 
 		$unitPrice=$this->item_detail_model->fetch($itemDetailId[0]);
-		$ctr = 0; //initialize the array to the index
+		$ctr = 1; //initialize the array to the index
 		$totalPrice = 0;
 
-		if ($vat != null){
-			foreach ($vat as $no){
-				$isVAT[$no - 1]=1;
-			}
+		foreach ($vatquery as $no){
+			$isVAT[substr($no,4)] = 1;
 		}
 
 		while (isset($itemDetailId[$ctr])){
-				$sales= new Sales_model();
-				$sales ->salesTransactionId = $salesTransactionId;
-				$sales ->itemDetailId = $itemDetailId[$ctr];
-				$unitPrice = $this->item_detail_model->fetch($itemDetailId[$ctr]);
-				$sales ->unitPrice = $unitPrice[0]['sellingPrice'];
-				$sales ->qty = $qty[$ctr];
-				$sales ->discount = $discount[$ctr];
-				$sales ->storeId = $storeId;
-				if (isset($isVAT[$ctr])){
-					$sales ->isVAT = $isVAT[$ctr];
-				}
-				$totalPrice = $totalPrice + $qty[$ctr] * $unitPrice[0]['sellingPrice'] -$discount[$ctr];
+			$sales= new Sales_model();
+			$sales ->salesTransactionId = $salesTransactionId;
+			$sales ->itemDetailId = $itemDetailId[$ctr];
+			$unitPrice = $this->item_detail_model->fetch($itemDetailId[$ctr]);
+			$sales ->unitPrice = $unitPrice[0]['sellingPrice'];
+			$sales ->qty = $qty[$ctr];
+			$sales ->discount = $discount[$ctr];
+			$sales ->storeId = $storeId;
+			if (isset($isVAT[$ctr])){
+				$sales ->isVAT = $isVAT[$ctr];
+			}
+			else{
+				$sales ->isVAT = 0;
+			}
 
-				$ctr++;
-				//debug :: dump($totalPrice);
-		}
+			$totalPrice = $totalPrice + $qty[$ctr] * $unitPrice[0]['sellingPrice'] -$discount[$ctr];
+			$ctr++;
 		$sales_transaction ->salesTransactionId = $salesTransactionId;
 		$sales_transaction ->totalPrice = $totalPrice;
 		$this->sales_transaction_model->save($sales_transaction);
-		//debug :: dump('success');*/
+		$this->sales_model->save($sales);
+		debug :: dump('success');
+		}
 	}
-
-
  }
