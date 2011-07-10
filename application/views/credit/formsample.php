51444d83ef104e6badc56e09ef9c6fe42632b401
Erick Masanque <erickmasanque@gmail.com>
2011-07-10 11:32:42 +0800

    deleted the creditform and replaced with credit payment form

--- application/views/credit/formsample.php
+++
@@ -1,5 +0,0 @@
-<form action="/credit/formsample" method="POST">
-	<label for="name">name </label><input id="name" type="text" name="name"/>
-	<label for="password">password: </label><input id="password" type="text" name="password"/>
-	<input type="submit">
-</form>
