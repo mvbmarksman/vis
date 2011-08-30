6ffe80e0a9716957cd992fc83ccc0166e9729c82
Mark <mvb_marksman@yahoo.com>
2011-08-30 22:42:26 +0800

    minor js msg changes

--- public/js/admin.js
+++ public/js/admin.js
@@ -4,6 +4,7 @@ var MSG_FADE = 1000;
 
 function flashMsg(message)
 {
+	$("#errorMsg").hide();
 	$("#flashMsg").html(message);
 	$("#flashMsg").show();
 	$("#flashMsg").delay(MSG_DELAY).fadeOut(MSG_FADE);
@@ -12,6 +13,7 @@ function flashMsg(message)
 
 function flashError(message)
 {
+	$("#flashMsg").hide();
 	$("#errorMsg").html(message);
 	$("#errorMsg").show();
 	$("#errorMsg").delay(MSG_DELAY).fadeOut(MSG_FADE);
